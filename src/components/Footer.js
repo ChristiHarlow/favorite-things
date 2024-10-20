67ecc230296052d0f59a20ce27ea84265e708ce2
ChristiHarlow <cah120671@gmail.com>
2024-10-20 09:28:28 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../styles/Footer.css'; // Optional if you're using a CSS file for styling
 
 const Footer = () => {
-  return <footer>Ⓒ Christi A. Harlow 2024</footer>;
+  return <footer>Ⓒ Christi's Favorite Things 2024</footer>;
 };
 
 export default Footer;
