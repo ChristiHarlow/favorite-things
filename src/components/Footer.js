55a6eed183c1dcff21b52bd82dd49af153e7dd44
ChristiHarlow <cah120671@gmail.com>
2025-01-20 23:18:40 -0500

    Spelling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../styles/Footer.css'; // Optional if you're using a CSS file for styling
 
 const Footer = () => {
-  return <footer>Ⓒ Christi's Favorite Things 2024</footer>;
+  return <footer>Ⓒ Christi's Favorite Things 2025</footer>;
 };
 
 export default Footer;
