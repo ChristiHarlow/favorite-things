cd07364a6ad93490ff0030f56d98cfa9eada145c
ChristiHarlow <103293099+ChristiHarlow@users.noreply.github.com>
2025-09-04 21:14:48 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Favorites from './Favorites';
 function App() {
   return (
     <div className="App">
+       <h1 className="page-title">Christi’s Favorite Things</h1>
       <Favorites />
     </div>
   );
